d561a6ec77071080502fd7fe33d07d617f2a8dc1
Romain Voivenel <voivenelromain@gmail.com>
2025-03-24 18:02:40 +0100

    ajout provider restaurant

---
+++ lib/bd/restaurant_provider.dart
@@ -0,0 +1,47 @@
+
+
+import 'package:sae_mobile/model/restaurant.dart';
+
+class RestaurantProvider {
+  var db;
+
+  RestaurantProvider({required this.db});
+
+  // Selects
+  Future<List<Restaurant>> restaurants() async {
+    return await db.query('RESTAURANT');
+  }
+
+  Future<Restaurant> restaurant(int id) async {
+    return await db.query(
+        'RESTAURANT',
+        where: 'idRestaurant = ?',
+        whereArgs: [id]
+    );
+  }
+
+  // Inserts
+  Future<Restaurant> insert(Restaurant r) async {
+    r.id = await db.insert("RESTAURANT", r.toMap());
+    return r;
+  }
+
+  // Update
+  Future<int> update(Restaurant r) async {
+    return await db.update(
+        "RESTAURANT",
+        r.toMap(),
+        where: 'idRestaurant = ?',
+        whereArgs: [r.id]
+    );
+  }
+
+  // Delete
+  Future<int> delete(Restaurant r) async {
+    return await db.delete(
+      "RESTAURANT",
+      where: 'idRestaurant = ?',
+      whereArgs: [r.id]
+    );
+  }
+}
