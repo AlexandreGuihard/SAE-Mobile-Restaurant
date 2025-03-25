ef9c51d66690918d664929605b96e3aee29c0477
Alexandre Guihard <guihardalex98@gmail.com>
2025-03-25 15:44:23 +0100

    Premiers inserts

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:sae_mobile/bd/tables.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
